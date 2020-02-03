0f01fd5d5c8866cf73239dcd32d5a2c8b3ddf754
Rishin Goswami <rishin.goswami@gmail.com>
2020-02-03 11:17:16 +0530

    Added array test. Restricted array types.

--- src/main/java/com/algowebsolve/webapp/model/TestPrimitiveArrays.java
+++ src/main/java/com/algowebsolve/webapp/model/TestPrimitiveArrays.java
@@ -3,7 +3,8 @@ package com.algowebsolve.webapp.model;
 import java.util.List;
 
 public class TestPrimitiveArrays {
-    public byte[] aByteArray;
+    //public byte[] aByteArray; // TODO: Currently unsupported. The reason is that jackson converts this into a base64 string,
+                                //        however the json-schema generate by jackson is of type [integer]
     public short[] aShortArray;
     public int[] anIntArray;
     public long[] aLongArray;
--- src/test/java/com/algowebsolve/webapp/WebappApplicationTests.java
+++ src/test/java/com/algowebsolve/webapp/WebappApplicationTests.java
@@ -1,6 +1,7 @@
 package com.algowebsolve.webapp;
 
 import com.algowebsolve.webapp.apiv1.Problems;
+import com.algowebsolve.webapp.model.TestPrimitiveArrays;
 import com.algowebsolve.webapp.model.TestPrimitives;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -40,7 +41,8 @@ class WebappApplicationTests { // TODO: rename to echo test
 		JsonNode respJson = response.block();
 		TestPrimitives respObj = jsonMapper.convertValue(respJson, TestPrimitives.class);
 		// assertions
-		Assertions.assertThat(obj.aByte).isEqualTo(respObj.aByte);
+		Assertions.assertThat(respJson).isEqualTo(jsonNode);
+		/*
 		Assertions.assertThat(obj.aShort).isEqualTo(respObj.aShort);
 		Assertions.assertThat(obj.anInt).isEqualTo(respObj.anInt);
 		Assertions.assertThat(obj.aLong).isEqualTo(respObj.aLong);
@@ -48,6 +50,30 @@ class WebappApplicationTests { // TODO: rename to echo test
 		Assertions.assertThat(obj.aDouble).isEqualTo(respObj.aDouble);
 		Assertions.assertThat(obj.aBoolean).isEqualTo(respObj.aBoolean);
 		Assertions.assertThat(obj.aChar).isEqualTo(respObj.aChar);
+		*/
+	}
+
+	@Test
+	void primitiveArrays() throws JsonProcessingException, JsonMappingException {
+		// fill obj value
+		TestPrimitiveArrays obj = new TestPrimitiveArrays();
+		//obj.aByteArray = new byte[]{13, 0, 1, -1, Byte.MAX_VALUE, Byte.MIN_VALUE};
+		obj.aShortArray = new short[]{133, 0, 1, -1, Short.MAX_VALUE, Short.MIN_VALUE};
+		obj.anIntArray = new int[]{1333, 0, 1, -1, Integer.MAX_VALUE, Integer.MIN_VALUE};
+		obj.aLongArray = new long[]{13333, 0, 1, -1, Long.MAX_VALUE, Long.MIN_VALUE};
+		obj.aFloatArray = new float[]{13.5f, 0, 1f, -1f, Float.MAX_VALUE, Float.MIN_VALUE, Float.NaN, Float.POSITIVE_INFINITY, Float.NEGATIVE_INFINITY};
+		obj.aDoubleArray = new double[]{13.55, 0.0, 1.0, -1.0, Double.MAX_VALUE, Double.MIN_VALUE, Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY};
+		obj.aBooleanArray = new boolean[]{true, true, false, true};
+		obj.aCharArray = "hello world".toCharArray();
+		// convert obj to json
+		String jsonString = jsonMapper.writeValueAsString(obj);
+		JsonNode jsonNode = jsonMapper.readTree(jsonString);
+		// request to response
+		Mono<JsonNode> response = controller.problemRequest("test-echo-primitivearrays", jsonNode);
+		JsonNode respJson = response.block();
+		TestPrimitiveArrays respObj = jsonMapper.convertValue(respJson, TestPrimitiveArrays.class);
+		// assertions
+		Assertions.assertThat(respJson).isEqualTo(jsonNode);
 	}
 
 }
